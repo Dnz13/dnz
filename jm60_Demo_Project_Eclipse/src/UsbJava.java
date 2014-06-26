8ed178cd1b36ff3e6cbf6af476409f9eb86c6123
Damla Deniz Akcan <denizdamla13@gmail.com>
2014-06-26 11:25:33 +0300

    Github revert denemesi

--- jm60_Demo_Project_Eclipse/src/UsbJava.java
+++ jm60_Demo_Project_Eclipse/src/UsbJava.java
@@ -3,16 +3,16 @@ import com.lucasF.JCommUSB_2_0.USBDevice;
 public class UsbJava {
 
 	public static void main(String[] args) {
-		//deneme 
+		//deneme 1
 		String szPath = "";
 		try{
 			szPath = USBDevice.getAttachedDevicePath(0);
-
+//			github revert denemesi 3
 			System.out.println("Device Path of Device at index 0 = "+ szPath);
 			USBDevice myUSB = new USBDevice(szPath);
 
 			System.out.println("connected?"+myUSB.deviceIsConnected()); 
-//			github deneme 
+//			github deneme 2
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
